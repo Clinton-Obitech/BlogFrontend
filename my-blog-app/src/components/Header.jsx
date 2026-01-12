76a6fda8af149903ec69294eef8a47b203a49ca3
obitech <clintcodeprogrammer@gmail.com>
2026-01-12 16:17:04 +0100

    yeah

--- my-blog-app/src/components/Header.jsx
+++ my-blog-app/src/components/Header.jsx
@@ -64,7 +64,7 @@ function Header() {
             <h3 style={{marginLeft: "0.2rem"}}>{user?.username ?? "..."}</h3>
           </div>
            : 
-          <NavLink style={{fontSize: "1.2rem", textDecoration: "none", color: "black"}} to='/Login'><i style={{color: "teal"}} className="fa-solid fa-user"></i> Sign in</NavLink>}
+          <NavLink style={{fontSize: "1.2rem", textDecoration: "none", color: "black"}} to='/login'><i style={{color: "teal"}} className="fa-solid fa-user"></i> Sign in</NavLink>}
           </div>
           </div>
 
