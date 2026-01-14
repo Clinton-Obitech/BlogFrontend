4cf71909e1067833612570dd677000752a98cdc5
obitech <clintcodeprogrammer@gmail.com>
2026-01-14 01:34:12 +0100

    hello

--- my-blog-app/src/api/axios.js
+++ my-blog-app/src/api/axios.js
@@ -1,8 +1,27 @@
 import axios from "axios";
+import { useLoader } from "../context/LoaderContext";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 });
+
+export const attachLoaderInterceptors = (setLoading) => {
+  api.interceptors.request.use((config) => {
+    setLoading(true);
+    return config;
+  });
+
+  api.interceptors.response.use(
+    (res) => {
+      setLoading(false);
+      return res;
+    },
+    (err) => {
+      setLoading(false);
+      return Promise.reject(err);
+    }
+  );
+};
 
 export default api;
---
+++ my-blog-app/src/components/GlobalLoader.jsx
@@ -0,0 +1,11 @@
+import "./loader.css";
+
+const GlobalLoader = () => {
+  return (
+    <div className="loader-overlay">
+      <div className="spinner" />
+    </div>
+  );
+};
+
+export default GlobalLoader;
