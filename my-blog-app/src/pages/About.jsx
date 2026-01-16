1398161efae0ead67c935d7521c80a295cf22038
obitech <clintcodeprogrammer@gmail.com>
2026-01-16 15:44:16 +0100

    oya

--- my-blog-app/src/pages/About.jsx
+++ my-blog-app/src/pages/About.jsx
@@ -1,7 +1,24 @@
+import styles from "./Page.module.css";
+
 export default function About() {
     return (
-        <>
+        <div className={styles.tacp}>
         <h2>About Us</h2>
-        </>
+        <p>
+    <strong>[Your Blog Name]</strong> is a blog created to share informative, engaging, and well-researched content on topics that matter.
+  </p>
+
+  <p>
+    Our goal is to educate, inform, and inspire readers by providing clear and reliable articles, updates, and insights.
+  </p>
+
+  <p>
+    We value honesty, creativity, and quality, and we are committed to delivering content that adds value to our audience.
+  </p>
+
+  <p>
+    Thank you for visiting and being part of our community.
+  </p>
+        </div>
     )
 }
